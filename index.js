276d48f4e1a900bed533299fc6718f1489591225
François Revol <revol@free.fr>
2017-03-07 23:49:46 +0100

    Update to new Duniter API
    Heavily based on Remuniter code.
    Thanks Eloïs for the hint!

--- index.js
+++ index.js
@@ -9,185 +9,187 @@ const path    = require('path');
 const express = require('express');
 const geoip   = require('geoip-lite');
 const natUpnp = require('nnupnp');
+const webserver = require('./lib/webserver.js');
+
 
 var upnpClient = natUpnp.createClient();
 
+// Default Duniter node's database
 const HOME_DUNITER_DATA_FOLDER = 'duniter_map';
 
-// Use netobs data folder
-if (!process.argv.includes('--mdb')) {
-	process.argv.push('--mdb');
-	process.argv.push(HOME_DUNITER_DATA_FOLDER);
+// Default host on which the map is available
+const DEFAULT_HOST = 'localhost';
+
+// Default port on which the map is available
+const DEFAULT_PORT = 10500;
+
+var SERVER_HOST;
+var SERVER_PORT;
+var peerTable = {};
+
+function inPeers(pubkey) {
+  if (pubkey in peerTable)
+    return true;
+  return false;
+}
+function addPeer(pubkey, geo, addresses, port, status) {
+  peerTable[pubkey] = {
+     "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [geo.ll[1], geo.ll[0]]
+    },
+    "properties": {
+      "name": "_Peer " + pubkey,
+      "pubkey": pubkey,
+      "addresses": addresses,
+      "port": port,
+      "status": status,
+      "description": "Peer " + pubkey + " (" + status + ")"
+    }
+  };
 }
 
-// Default action = start
-if (process.argv.length === 4) process.argv.push('start');
+const stack = duniter.statics.autoStack([{
+  name: 'duniter-map',
+  required: {
 
-// Disable Duniter logs
-duniter.statics.logger.mute();
+    duniter: {
 
-duniter.statics.cli((duniterServer) => co(function*() {
+      cliOptions: [
+      ],
 
-	const HOST = 'localhost';
-	const PORT = 10500;
+      cli: [{
+        name: 'duniter-map [host] [port]',
+        desc: 'Starts duniter-map node',
 
-	try {
+        // Disables Duniter node's logs
+        //logs: false,
 
-	const app = express();
+        onDatabaseExecute: (duniterServer, conf, program, params, startServices) => co(function*() {
+          SERVER_HOST = params[0] || DEFAULT_HOST;
+          SERVER_PORT = parseInt(params[1]) || DEFAULT_PORT;
 
-	var r = upnpClient.portMapping({
-		'public': PORT,
-		'private': PORT,
-		ttl: 10
-	}, function(err) {
-		// Will be called once finished
-		console.log(err);
-	});
-	console.log(r);
+          var r = upnpClient.portMapping({
+            'public': SERVER_PORT,
+            'private': SERVER_PORT,
+            ttl: 10
+          }, function(err) {
+            // Will be called once finished
+            console.log(err);
+          });
+          console.log(r);
 
-	var peerTable = {};
+          console.log("Resolving initial known peers...");
+          try {
+            const peers = yield duniterServer.dal.peerDAL.listAll();
+            //console.log(peers);
+            var data;
+            for (data of peers) {
+              //var data = peers[pe];
+              var status = data.status;
+              var ipv4 = null;
+              var ipv6 = null;
+              var addresses = [];
+              var port = "0";
+              var ep;
+              //TODO: try next one if first resolve failed
+              //TODO: handle IPv6 as well
+              for (ep of data.endpoints) {
+                var s;
+                var eps = ep.split(' ');
+                if (eps.shift() != 'BASIC_MERKLED_API')
+                  continue;
+                port = parseInt(eps.pop());
+                for (s of eps) {
+                  addresses.push(s);
+                  if (s.match(/^\d+\.\d+\.\d+\.\d+$/))
+                    ipv4 = s;
+                  if (s.match(/^[0-9a-fA-F]{1,4}:/))
+                    ipv6 = s;
+                }
+              }
+              //console.log(addresses);
+              //console.log(ipv4);
+              //console.log(ipv6);
+              if ((ipv4 || ipv6) && !inPeers(data.pubkey)) {
+                var geo = null;
+                if (ipv4)
+                  geo = geoip.lookup(ipv4);
+                else if (ipv6)
+                  geo = geoip.lookup(ipv6);
+                //console.log(geo);
+                if (geo)
+                  addPeer(data.pubkey, geo, addresses, port, status);
+              }
+            }
+          } catch (e) {
+            // En cas d'exception, afficher le message
+            console.log(e);
+          }
 
-	function inPeers(pubkey) {
-		if (pubkey in peerTable)
-			return true;
-		return false;
-	}
-	function addPeer(pubkey, geo, addresses, port, status) {
-		peerTable[pubkey] = {
-			 "type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [geo.ll[1], geo.ll[0]]
-			},
-			"properties": {
-				"name": "_Peer " + pubkey,
-				"pubkey": pubkey,
-				"addresses": addresses,
-				"port": port,
-				"status": status,
-				"description": "Peer " + pubkey + " (" + status + ")"
-			}
-		};
-	}
+          // Web server
+          let httpServer = webserver(SERVER_HOST, SERVER_PORT, duniterServer, peerTable);
+          yield httpServer.openConnection();
 
-	const staticContentPath = path.join(__dirname, './static');
-	app.use(express.static(staticContentPath));
+          console.log("Web server at http://%s:%s", SERVER_HOST, SERVER_PORT);
+          console.log("GeoJSON data at http://%s:%s/peers.geojson", SERVER_HOST, SERVER_PORT);
 
-	/**
-	 * Sur appel de l'URL /abc
-	 */
-	app.get('/peers.geojson', (req, res) => co(function *() {
-		try {
-			var json = {
-				"type": "FeatureCollection",
-				"features": []
-			};
-			var p;
-			for (p in peerTable) {
-				json.features.push(peerTable[p]);
-			}
-			res.type('application/geo+json');
-			res.setHeader("Access-Control-Allow-Origin", "*");
-			res.status(200).send(json);
-		} catch (e) {
-			// En cas d'exception, afficher le message
-			res.status(500).send('<pre>' + (e.stack || e.message) + '</pre>');
-		}
-	}));
+          // IMPORTANT: release Duniter services from "sleep" mode
+          yield startServices();
 
+          // Forward blocks
+          duniterServer
+            .pipe(es.mapSync(function(data) {
+              if (data.documentType == 'peer') {
+                console.log('>> New peer document !');
+                //console.log(data);
+                var ipv4 = data.getIPv4();
+                var ipv6 = data.getIPv6();
+                var port = data.getPort();
+                var addresses = [];
+                if (ipv4)
+                  addresses.push(ipv4);
+                if (ipv6)
+                  addresses.push(ipv6);
+                //console.log(addresses);
+                //console.log(ipv4);
+                //console.log(ipv6);
+                if ((ipv4 || ipv6) && !inPeers(data.pubkey)) {
+                  var geo = null;
+                  if (ipv4)
+                    gep = geoip.lookup(ipv4);
+                  else if (ipv6)
+                    gep = geoip.lookup(ipv6);
+                  //console.log(geo);
+                  if (geo)
+                    addPeer(data.pubkey, geo, addresses, port, "UP");
+                }
+              }
+              else if (data.documentType == 'block')
+                console.log('>> New block !');
+              else
+                console.log('>> New data !');
+            }));
 
-	console.log("Resolving initial known peers...");
-	try {
-		const peers = yield duniterServer.dal.peerDAL.listAll();
-		//console.log(peers);
-		var data;
-		for (data of peers) {
-			//var data = peers[pe];
-			var status = data.status;
-			var ipv4 = null;
-			var ipv6 = null;
-			var addresses = [];
-			var port = "0";
-			var ep;
-			//TODO: try next one if first resolve failed
-			//TODO: handle IPv6 as well
-			for (ep of data.endpoints) {
-				var s;
-				var eps = ep.split(' ');
-				if (eps.shift() != 'BASIC_MERKLED_API')
-					continue;
-				port = parseInt(eps.pop());
-				for (s of eps) {
-					addresses.push(s);
-					if (s.match(/^\d+\.\d+\.\d+\.\d+$/))
-						ipv4 = s;
-					if (s.match(/^[0-9a-fA-F]{1,4}:/))
-						ipv6 = s;
-				}
-			}
-			//console.log(addresses);
-			//console.log(ipv4);
-			//console.log(ipv6);
-			if ((ipv4 || ipv6) && !inPeers(data.pubkey)) {
-				var geo = null;
-				if (ipv4)
-					geo = geoip.lookup(ipv4);
-				else if (ipv6)
-					geo = geoip.lookup(ipv6);
-				//console.log(geo);
-				if (geo)
-					addPeer(data.pubkey, geo, addresses, port, status);
-			}
-		}
-	} catch (e) {
-		// En cas d'exception, afficher le message
-		console.log(e);
-	}
+          // Wait forever, Remuniter is a permanent program
+          yield new Promise(() => null);
+        })
+      }]
+    }
+  }
+}]);
 
-
-	const httpServer = http.createServer(app);
-	httpServer.listen(PORT, HOST);
-	console.log("Web server at http://%s:%s", HOST, PORT);
-	console.log("GeoJSON data at http://%s:%s/peers.geojson", HOST, PORT);
-
-	duniterServer.pipe(es.mapSync((data) => {
-		if (data.documentType == 'peer') {
-			console.log('>> New peer document !');
-			//console.log(data);
-			var ipv4 = data.getIPv4();
-			var ipv6 = data.getIPv6();
-			var port = data.getPort();
-			var addresses = [];
-			if (ipv4)
-				addresses.push(ipv4);
-			if (ipv6)
-				addresses.push(ipv6);
-			//console.log(addresses);
-			//console.log(ipv4);
-			//console.log(ipv6);
-			if ((ipv4 || ipv6) && !inPeers(data.pubkey)) {
-				var geo = null;
-				if (ipv4)
-					gep = geoip.lookup(ipv4);
-				else if (ipv6)
-					gep = geoip.lookup(ipv6);
-				//console.log(geo);
-				if (geo)
-					addPeer(data.pubkey, geo, addresses, port, "UP");
-			}
-		}
-		else if (data.documentType == 'block')
-			console.log('>> New block !');
-		else
-			console.log('>> New data !');
-	}));
-
-	/****************************************/
-
-	} catch (e) {
-		console.error(e);
-		console.log('Removing UPnP NAT mapping...');
-		upnpClient.portUnmapping({'public': PORT});
-		process.exit(1);
-	}
-}));
+co(function*() {
+  if (!process.argv.includes('--mdb')) {
+    // We use the default database
+    process.argv.push('--mdb');
+    process.argv.push(HOME_DUNITER_DATA_FOLDER);
+  }
+  // Execute our program
+  yield stack.executeStack(process.argv);
+  console.log('Removing UPnP NAT mapping...');
+  upnpClient.portUnmapping({'public': SERVER_HOST});
+  // End
+  process.exit();
+});
